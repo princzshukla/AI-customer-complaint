58ae7ae99a9404a8eab67335ff0e6fa5ce3eb034
Princzshukla <shrutishukla60870@gmail.com>
2026-08-09 00:14:45 +0530

    fix: add TypeScript ignore comment for CSS import in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import App from './App.tsx';
+// @ts-ignore: side-effect import of CSS without type declarations
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
